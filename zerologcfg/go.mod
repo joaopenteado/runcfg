5faa6569a60cfa87a871a7f29371cff0ab93bb85
joaopenteado <4219131+joaopenteado@users.noreply.github.com>
2025-05-03 20:03:18 +0900

    Add `zerologcfg` as a submodule

---
+++ zerologcfg/go.mod
@@ -0,0 +1,15 @@
+module github.com/joaopenteado/runcfg/zerologcfg
+
+go 1.24.1
+
+require (
+	github.com/rs/zerolog v1.34.0
+	go.opentelemetry.io/otel/trace v1.35.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ zerologcfg/log.go
@@ -0,0 +1,78 @@
+package zerologcfg
+
+import (
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func init() {
+	// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+	zerolog.LevelFieldName = "severity"
+	zerolog.LevelFieldMarshalFunc = func(l zerolog.Level) string {
+		switch l {
+		case zerolog.TraceLevel:
+			return "DEFAULT"
+		case zerolog.DebugLevel:
+			return "DEBUG"
+		case zerolog.InfoLevel:
+			return "INFO"
+		case zerolog.WarnLevel:
+			return "WARNING"
+		case zerolog.ErrorLevel:
+			return "ERROR"
+		case zerolog.FatalLevel:
+			return "CRITICAL"
+		case zerolog.PanicLevel:
+			return "ALERT"
+		case zerolog.NoLevel:
+			return "DEFAULT"
+		default:
+			return "DEFAULT"
+		}
+	}
+}
+
+type cloudLoggingHook struct {
+	ProjectID string
+}
+
+func Hook(projectID string) zerolog.Hook {
+	return &cloudLoggingHook{ProjectID: projectID}
+}
+
+func (h cloudLoggingHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
+	// https://cloud.google.com/logging/docs/structured-logging
+
+	// CallerSkipFrameCount is the number of stack frames to skip to find the
+	// caller. The first frame is the CloudLoggingHook, the second is the
+	// zerolog.Logger, the third is the caller.
+	const skipFrameCount = 3
+
+	var file, line, function string
+	if pc, filePath, lineNum, ok := runtime.Caller(skipFrameCount); ok {
+		if f := runtime.FuncForPC(pc); f != nil {
+			function = f.Name()
+		}
+		line = strconv.Itoa(lineNum)
+		file = filePath[strings.LastIndexByte(filePath, '/')+1:]
+	}
+	e.Dict("logging.googleapis.com/sourceLocation", zerolog.Dict().
+		Str("file", file).
+		Str("line", line).
+		Str("function", function))
+
+	ctx := e.GetCtx()
+	if span := trace.SpanContextFromContext(ctx); span.IsValid() {
+		e.Str("logging.googleapis.com/trace", "projects/"+h.ProjectID+"/traces/"+span.TraceID().String())
+		e.Str("logging.googleapis.com/spanId", span.SpanID().String())
+		e.Bool("logging.googleapis.com/trace_sampled", span.TraceFlags().IsSampled())
+	}
+}
